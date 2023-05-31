0c141ab99a45f2a27f2b2a1b20d2d4d5c92f0182
Elodie Angenard <ange.elodie@gmail.com>
2023-05-31 17:08:42 +0200

    suppression des target_blank

---
+++ header_sommaire_comm.php
@@ -0,0 +1,14 @@
+<header>
+    <nav>
+        <a href="sommaire_general.html">Accueil</a>
+    </nav>
+
+    <nav>
+        <a href="competences_comm.html">Compétences qui améliorent la communication</a>
+        <a href="savoir_etre.html">Savoir être</a>
+        <a href="CNV.html">Communication Non Violente</a>
+        <a href="comm_efficace.html">Communication efficace</a>
+        <a href="styles_comm.html">Styles de communication</a>
+        <a href="prise_de_parole.html">Prise de parole en public</a>
+    </nav>
+</header>
---
+++ header_sommaire_general.php
@@ -0,0 +1,3 @@
+<nav>
+    <a href="sommaire_general.html" target="_blank">Accueil</a>
+</nav>
