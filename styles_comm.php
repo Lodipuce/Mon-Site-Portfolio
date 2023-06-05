bc8494118c085180e66a35613a896437d3440b80
Elodie Angenard <ange.elodie@gmail.com>
2023-06-05 18:13:54 +0200

    structure styles_comm

---
+++ styles_comm.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Styles de communication</title>
+    </head>
+
+    <body>
+        <?php include('header_sommaire_comm.php'); ?>
+
+        <h1>Styles de communication</h1>
+
+        <ul>
+            <li><a href="#direct">Style direct</a></li>
+            <li><a href="#initiateur">Style initiateur</a></li>
+            <li><a href="#encourageant">Style encourageant</a></li>
+            <li><a href="#analytique">Style analytique</a></li>
+            <li><a href="#adapter">S'adapter aux autres styles de communication</a></li>
+            <li><a href="#monAvis">Mon avis</a></li>
+        </ul>
+
+        <section id="direct">
+            <h2>Style direct</h2>
+        </section>
+
+        <section id="initiateur">
+            <h2>Style initiateur</h2>
+        </section>
+
+        <section id="encourageant">
+            <h2>Style encourageant</h2>
+        </section>
+
+        <section id="analytique">
+            <h2>Style analytique</h2>
+        </section>
+
+        <section id="adapter">
+            <h2>S'adapter aux autres styles de communication</h2>
+        </section>
+
+        <section id="monAvis">
+            <h2>Mon avis</h2>
+        </section>
+
+    </body>
+</html>
