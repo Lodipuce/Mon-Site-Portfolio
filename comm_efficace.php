15de09922256286ec7a77e079d4dddaac0c0112a
Elodie Angenard <ange.elodie@gmail.com>
2023-06-05 18:03:50 +0200

    structure comm_efficace

---
+++ comm_efficace.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Communication efficace</title>
+    </head>
+
+    <body>
+        <?php include('header_sommaire_comm.php'); ?>
+
+        <h1>Communication efficace</h1>
+
+        <ul>
+            <li><a href="#cercle">Le cercle d'or</a></li>
+            <li><a href="#corporel">Le langage non verbal</a></li>
+            <li><a href="#questions">Les questions puissantes</a></li>
+            <li><a href="#methode">La méthode des 6C</a></li>
+            <li><a href="#feedback">Le feedback</a></li>
+            <li><a href="#feedforward">Le feedforward</a></li>
+            <li><a href="#monAvis">Mon avis</a></li>
+        </ul>
+
+        <section id="cercle">
+            <h2>Le cercle d'or</h2>
+        </section>
+
+        <section id="corporel">
+            <h2>Le langage non verbal</h2>
+        </section>
+
+        <section id="questions">
+            <h2>Les questions puissantes</h2>
+        </section>
+
+        <section id="methode">
+            <h2>La méthode des 6C</h2>
+        </section>
+
+        <section id="feedback">
+            <h2>Le feedback</h2>
+        </section>
+
+        <section id="feedforward">
+            <h2>Le feedforward</h2>
+        </section>
+
+        <section id="monAvis">
+            <h2>Mon Avis</h2>
+        </section>
+
+    </body>
+</html>
