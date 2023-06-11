d64f71fdddb41be6d8aed81d610d362d9d516603
Elodie Angenard <ange.elodie@gmail.com>
2023-06-11 13:54:06 +0200

    cercler d'or + questions puissantes

--- comm_efficace.php
+++ comm_efficace.php
@@ -24,6 +24,16 @@
 
         <section id="cercle">
             <h2>Le cercle d'or</h2>
+            <p>Le cercle d'or est une notion élaborée par Simon Sinek, auteur et conférencier. Ce cercle comporte 3 couches : <strong>Quoi</strong>, <strong>Comment</strong>, <strong>Pourquoi</strong>.</p>
+            <p>La plupart des entreprises dans leur marketing, ou la plupart des gens, parlent de leur <strong>Quoi</strong>. Qu'est-ce que tu fais ? Je fais ça.</p>
+            <p>Simon Sinek a étudié les entreprises comme Apple et les grands communicants comme Martin Luther King pour comprendre ce qui les différenciait. Ils ne commencent pas par leur <strong>Quoi</strong> mais par leur <strong>Pourquoi</strong> !</p>
+            <p>Ainsi, en commençant par le <strong>Pourquoi</strong>, on explique la raison qui nous pousse à faire ce que l'on fait, pourquoi on a choisi telle ou telle voie. C'est notre raison d'être. On devient inspirant.</p>
+            <p>La théorie du cercle d'or repose en fait sur un simple mot : l'<strong>authenticité</strong>. Raconter son histoire, sa raison d'être, qui on est, ce qu'on fait et pourquoi on le fait.</p>
+            <aside>Pour en savoir plus : <a href="https://flexjob.fr/why-theorie-cercle-dor-simon-sinek/" target="_blank">flexjob.fr</aside>
+            <figure>
+                <img src="./img/cercledor.png" alt="cercle d'or"/>
+                <figcaption>Source : <a href="https://www.squid-impact.fr/communiquez-ce-que-vous-etes" target="_blank">squid impact</a></figcaption>
+            </figure>
         </section>
 
         <section id="corporel">
@@ -32,6 +42,26 @@
 
         <section id="questions">
             <h2>Les questions puissantes</h2>
+            <p>Une question puissante est une question ouverte, c'est à dire que l'on ne peut pas y répondre par oui ou non. Les réponses attendues seront donc construites et structurées pour trouver une solution et ne pas rester bloqué sur un problème.</p>
+            <figure>
+                <img src="./img/thinkOutOfTheBox.jpg" alt="Penser en dehors de la boîte"/>
+                <figcaption>Source : <a href="https://pauldevaux.fr/question-puissante/" target="_blank">pauldevaux.fr</a></figcaption>
+            </figure>
+            <p>Ces questions sont impliquantes. Elles permettent de contruire un échange entre vous et votre interlocuteur.</p>
+            <figure>
+                <img src="./img/caracteristiquesQuestionsPuissantes.jpg" alt="Caractéristiques des questions puissantes"/>
+                <figcaption>Source : Deastance Services</figcaption>
+            </figure>
+            <figure>
+                <img src="./img/impactsQuestionsPuissantes.jpg" alt="Impact des questions puissantes"/>
+                <figcaption>Source : Deastance Services</figcaption>
+            </figure>
+            <p>Quelques exemples de questions puissantes :</p>
+            <figure>
+                <img src="./img/exemplesQuestionsPuissantes.jpg" alt="Exemples de questions puissantes"/>
+                <figcaption>Source : Deastance Services</figcaption>
+            </figure>
+            
         </section>
 
         <section id="methode">
