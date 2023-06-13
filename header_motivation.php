6379348cb46147996d06083418b301d4c6859440
Elodie Angenard <ange.elodie@gmail.com>
2023-06-13 18:21:51 +0200

    début motivation, suppression pages cv citoyen et cv motivationnel (tout sera sur la même page cv)

--- header_motivation.php
+++ header_motivation.php
@@ -6,8 +6,6 @@
 
     <nav>
         <a href="cv.php">CV</a>
-        <a href="cv_citoyen.php">CV citoyen</a>
-        <a href="cv_motivationnel.php">CV motivationnel</a>
         <a href="entretien.php">Entretien d'embauche</a>
     </nav>
 </header>
--- motivation.php
+++ motivation.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="stylesheet" href="styles.css"> -->
+    <link rel="stylesheet" href="styles.css">
     <title>Motivation</title>
 </head>
 
@@ -13,6 +13,53 @@
 
         
         <h1>Exprimer sa motivation</h1>
+
+        <p>Afin de décrocher un entretien d'embauche, il faut savoir  <strong>identifier les motivations</strong> qui nous poussent à postuler à ce poste précis, à répondre à cette annonce. Ensuite, il faut savoir formuler un <strong>mail de motivation</strong>, qui sera rédigé différemment de la <strong>lettre de motivation</strong>. Enfin, quand l'attention du recruteur est attirée, il peut arriver de devoir faire un premier contact par <strong>appel téléphonique</strong>.</p>
+        <p>J'aborde ces points sur cette page puis je détaillerai plus les <a href="cv.php">CV</a> et <a href="entretien.php">l'entretien d'embauche</a>.</p>
+
+        <ul>
+            <li><a href="#identifier">Comment identifier mes motivations ?</a></li>
+            <li><a href="#mail">Email de motivation</a></li>
+            <li><a href="#lettre">Lettre de motivation</a></li>
+            <li><a href="#appel">Appel téléphonique</a></li>
+        </ul>
+
+        <figure class="mindmap">
+            <img src="./img/Exprimer sa motivation.png" alt="Mindmap exprimer sa motivation"/>
+        </figure>
+
+        <section id="identifier">
+            <h2>Comment identifier mes motivations ?</h2>
+            <figure class="mindmap">
+                <img src="./img/motivations_identifier.jpg" alt="Mindmap identifier mes motivations"/>
+            </figure>
+            <p>Pour identifier nos motivations pour un poste, nous devons analyser l'offre l'embauche et nous renseigner sur l'entreprise. C'est ainsi que nous saurons si nos ambitions, nos valeurs, nos objectifs, concordents avec ceux du poste et de l'entreprise.</p>
+        </section>
+
+        <section id="mail">
+            <h2>Email de motivation</h2>
+            <figure class="mindmap">
+                <img src="./img/motivations_email.jpg" alt="Mindmap identifier mes motivations"/>
+            </figure>
+        </section>
+
+        <section id="lettre">
+            <h2>Lettre de motivation</h2>
+            <figure class="mindmap">
+                <img src="./img/motivation_lettre.jpg" alt="Mindmap identifier mes motivations"/>
+            </figure>
+
+            <figure class="mindmap">
+                <img src="./img/candidature_spontanee.jpg" alt="Mindmap identifier mes motivations"/>
+            </figure>
+        </section>
+
+        <section id="appel">
+            <h2>Appel téléphonique</h2>
+            <figure class="mindmap">
+                <img src="./img/motivations_appel.jpg" alt="Mindmap identifier mes motivations"/>
+            </figure>   
+        </section>
         
 
         <?php include('footer.php'); ?>
